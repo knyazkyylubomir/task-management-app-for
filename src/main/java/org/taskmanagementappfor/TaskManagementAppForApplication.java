a29c104235be2bd1980d8e83ea48ae4dc1b4edb6
Liubomyr Kniazkyi <knyazkyylubomir@gmail.com>
2024-04-25 17:55:33 +0300

    created infrastructure for the project

---
+++ src/main/java/org/taskmanagementappfor/TaskManagementAppForApplication.java
@@ -0,0 +1,13 @@
+package org.taskmanagementappfor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementAppForApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementAppForApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/taskmanagementappfor/TaskManagementAppForApplicationTests.java
@@ -0,0 +1,13 @@
+package org.taskmanagementappfor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementAppForApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
